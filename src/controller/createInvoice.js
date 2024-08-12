372bdd811ddd8f0ded9c28302413247dffae0089
Amr Mohamed <amrmuhmedd@gmail.com>
2024-08-12 11:16:15 +0300

    fix: create invoice error

--- src/controller/createInvoice.js
+++ src/controller/createInvoice.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
 export async function createInvoice(req, res) {
-  const response = await axios.post(
-    "https://test-api.kashier.io/paymentRequest?currency=EGP",
-    {
-      ...req.body,
-    },
-    {
-      headers: {
-        Authorization: `${process.env.KASHIER_SECERT}`,
+  await axios
+    .post(
+      "https://test-api.kashier.io/paymentRequest?currency=EGP",
+      {
+        ...req.body,
       },
-    }
-  );
-  console.log(response.data);
-  res.send(response.data);
+      {
+        headers: {
+          Authorization: `${process.env.KASHIER_SECERT}`,
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    });
 }
